8a2f7fafdd0377ef6cb9d28626b9c65d3bed0e56
rafaprates <rafaelbprates@gmail.com>
2020-09-27 11:57:12 -0400

    new design

--- connector.py
+++ connector.py
@@ -13,52 +13,30 @@ def write_to_database(data):
     cnx = mysql.connector.connect(user='pi', password='password', database='consumption_data')
     cursor = cnx.cursor()
 
-
-    add_data = ('INSERT INTO equipment_kwh '
-            '(equipment, kwh) '
+    insert_data = ('INSERT INTO 30s '
+            '(geladeira, chuveiro) '
             'VALUES (%s, %s)')
 
-    cursor.execute(add_data, data)
+    cursor.execute(insert_data, data)
     cnx.commit()
     cursor.close()
     cnx.close()
 
 
 def generate_random_data():
-    '''This function generates random data every set interval'''
+    '''This function generates random'''
 
-    # The keys are equipment names and the values are consumption in Kwh
-    data = {'geladeira': 0, 'chuveiro': 0,}
-    for key in data:
-        data[key] = random.randint(0, 1000) 
-    #data['agora'] = datetime.now().date()
+    data = []
+    for i in range(2):
+        data.append(random.randint(0, 1000))
     return data
 
 
-# The keys are equipment names and the values are consumption in Kwh
-#data = {'geladeira': 0, 'chuveiro': 0}
-
-    #data = generate_random_data()
-    #for key, value in data:
-    #    data = (key, value, agora) 
-
 while True:
     data = generate_random_data()
-    #print(data) 
-    for data in data.items(): 
-        write_to_database(data)
-        print(data)
-        print('###')
+    print(data) 
+    write_to_database(data)
     time.sleep(10)
-
-
-#writeToDatabase()
-
-
-
-
-
-
 
 
 
